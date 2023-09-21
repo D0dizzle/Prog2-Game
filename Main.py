a9becc36c985064a33e68885864f6257abab0691
L1zzard <lisa.sachau@student.fh-kiel.de>
2023-09-21 09:10:47 +0200

    Haha hab was geändert :)

--- Main.py
+++ Main.py
@@ -7,14 +7,18 @@ import sys
 
 #Variablen (todo: später hier drunter reinschreiben, haha)
 FPS = pygame.time.Clock()
+screen_hoehe = 400                  #hier setzen wir hoehe und breite des Spielfensters
+screen_breite = 500
+screen = pygame.display.set_mode(screen_hoehe, screen_breite)   #hier initialisieren wir das Spielfenster (erstmal ohne Grafik)
 
 #allgemeine Funktionen:
 #Spiel durch "x" in der Ecke schließen
 def exit_game():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()                       #Quelle für pygame.quit(): coderslegacy.com/python/python-pygame-tutorial
+            pygame.locals.quit()                       #Quelle für pygame.quit(): coderslegacy.com/python/python-pygame-tutorial
             sys.exit()
+
 
 
 #Platz für Klassen:
@@ -24,5 +28,8 @@ def exit_game():
 while True:
     #hier kommen die Events rein, auf die unser Programm in jedem Frame reagieren soll
 
+    exit_game()
     pygame.display.update()     #die Funktion macht, dass alle Änderungen im Spieler Screen angezeigt werden
+    #screen.blit(screen, (0,0))     brauchen wir, um den Screen von alten Grafiken zu bereinigen   
+    #screen.blit(player, neue position)      brauchen wir, um den Player-Sprite wieder anzuzeigen
     FPS.tick(60)                #60 bedeutet, dass das Spiel in 60 FPS angezeigt wird
--- spiel.py
+++ spiel.py
@@ -1,4 +1,4 @@
-"""Das hier ist für verschiedene Tests, nicht wundern!
+"""Das hier ist für verschiedene Tests gekapert, nicht wundern!
 Hier gibt's ne kleine Default MainLoop, in der man Funktionen ausprobieren kann"""
 
 import pygame as pg
@@ -9,4 +9,18 @@ FPS = pg.time.Clock()
 schwarz = pg.color(0, 0, 0)
 weiss = pg.color(255, 255, 255)
 
-bildschirm = pg.display.set_mode(300, 300)
+screen = pg.display.set_mode(300, 300)
+
+#Funktion, um Tastendruck des Spielers auszuwerten und entsprechende Aktionen auszuführen
+#kommt später vermutlich als Methode in die "Player"-Klasse
+
+def update(a):                                     #a ist die derzeitige Position des Player-Sprites
+    gedrueckte_Taste = pg.key.get_pressed()            
+    if gedrueckte_Taste[pg.K_UP]:                   #wir schauen: wurde Pfeiltaste nach oben gedrückt?
+        a.move_ip(0, -5)                            #wenn ja: bewegen wir den Spieler um -5 Pixel nach oben
+    if gedrueckte_Taste[pg.K_DOWN]:                  # hier 5 pixel nach unten
+        a.move_ip(0, 5)
+    if gedrueckte_Taste[pg.K_LEFT]:                 #-5 pixel nach links
+        a.move_ip(-5, 0)
+    if gedrueckte_Taste[pg.K_RIGHT]:                #5 pixel nach rechts
+        a.move_ip(5, 0)
