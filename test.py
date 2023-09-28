a9997925baf38e08c699e6073ac7a85e917b0412
D0dizzle <domenico-ferraro@web.de>
2023-09-28 13:37:37 +0200

    Umstrukturierung in test.py als Game class

--- test.py
+++ test.py
@@ -11,23 +11,15 @@ from abc import ABC, abstractclassmethod
 
 from pygame.sprite import* 
 
-pg.display.set_caption("Centipeter")
-FPS = pg.time.Clock()
-
-schwarz = (0, 0, 0)
-weiss = (255, 255, 255)
-cyan = (100, 100, 255)
-
 game_folder = os.path.dirname(__file__)
-screen = pg.display.set_mode(size=(500, 500))
 #TODO os.path.join um os path zu verallgrmrinrtn
 
 
-background1 = pg.image.load(os.path.join(game_folder, "Assets", "hintergrund", "parallax-background.png")).convert()
-background = pg.transform.scale(background1, (500, 500))
+'''background1 = pg.image.load(os.path.join(game_folder, "Assets", "hintergrund", "parallax-background.png")).convert()
+background = pg.transform.scale(background1, (1000, 1000))
 background2 = pg.image.load(os.path.join(game_folder,"Assets","hintergrund" ,"parallax-space-ring-planet.png")).convert_alpha()
 background3 = pg.image.load(os.path.join(game_folder,"Assets","hintergrund","parallax-space-stars.png")).convert_alpha()
-background4 = pg.transform.scale(background3, (500, 500))
+background4 = pg.transform.scale(background3, (1000, 1000))'''
 
 # Background Asset Quelle: https://opengameart.org/content/space-background-3
 
@@ -46,15 +38,74 @@ background4 = pg.transform.scale(background3, (500, 500))
     if gedrueckte_Taste[pg.K_RIGHT]:                #5 pixel nach rechts
         a.move_ip(5, 0)"""
 
-def exit_game():
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            pg.quit()                       #Quelle für pygame.quit(): coderslegacy.com/python/python-pygame-tutorial
-            sys.exit()
-
 #Klassen:
+class Game:
+
+    def __init__(self):
+        self.clock = pg.time.Clock()
+        self.title = pg.display.set_caption("Centipeter")
+        self.screen = pg.display.set_mode(size=(1000, 1000))
+        self.background1 = pg.image.load(os.path.join(game_folder, "Assets", "hintergrund", "parallax-background.png")).convert()
+        self.background = pg.transform.scale(self.background1, (1000, 1000))
+        self.background2 = pg.image.load(os.path.join(game_folder,"Assets","hintergrund" ,"parallax-space-ring-planet.png")).convert_alpha()
+        self.background3 = pg.image.load(os.path.join(game_folder,"Assets","hintergrund","parallax-space-stars.png")).convert_alpha()
+        self.background4 = pg.transform.scale(self.background3, (1000, 1000))
+
+        # Background Asset Quelle: https://opengameart.org/content/space-background-3
+
+
+    def new_game(self):
+        self.sprites = pg.sprite.Group()
+        self.players = pg.sprite.Group()
+        self.bullets = pg.sprite.Group()
+        self.player = Player(20, 20, 5, 5)
+        self.players.add(self.player)
+        self.run()
+    
+    def run(self):
+        while True:
+            self.exit_game()
+            pg.display.update()
+            self.clock.tick(60)
+            self.events()
+            self.update()
+            self.draw()
+
+    def events(self):
+        self.gedrueckte_Taste = pg.key.get_pressed()         
+                                    #wir schauen: wurde Pfeiltaste nach oben gedrückt?
+        if self.gedrueckte_Taste[pg.K_UP] and self.position.y > 400:
+            self.rect.move_ip(0, -1 * self.vy)                            #wenn ja: bewegen wir den Spieler um -5 Pixel nach oben
+        if self.gedrueckte_Taste[pg.K_DOWN] and self.position.y < 450:                     # hier 5 pixel nach unten
+            self.rect.move_ip(0, self.vy)
+        if self.gedrueckte_Taste[pg.K_LEFT] and self.position.x > 0:                 #-5 pixel nach links
+            self.rect.move_ip(-1 * self.vx, 0)
+        if self.gedrueckte_Taste[pg.K_RIGHT] and self.position.x < 450:                #5 pixel nach rechts
+            self.rect.move_ip(self.vx, 0)
+
+    def update(self):
+        self.sprites.update()
+        self.players.update()
+        self.bullets.update()
+
+    def draw(self):
+        self.screen.blit(self.background, (0,0))
+        self.screen.blit(self.background2, (20,50))
+        self.screen.blit(self.background4, (20,50))
+        self.screen.blit(self.sprites)
+        self.screen.blit(self.players)
+        self.screen.blit(self.bullets)
+
+
+        
+    def exit_game():
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()                       #Quelle für pygame.quit(): coderslegacy.com/python/python-pygame-tutorial
+                sys.exit()
+
 #Spieler Klasse
-class player(pg.sprite.Sprite):             #Quelle für Erstellungshilfe = pygame doc
+class Player(pg.sprite.Sprite):             #Quelle für Erstellungshilfe = pygame doc
     
     def __init__(self, x, y, vx, vy):
         super().__init__()
@@ -70,7 +121,7 @@ class player(pg.sprite.Sprite):             #Quelle für Erstellungshilfe = pyga
         
         
     
-    def update(self):
+    '''def update(self):
         gedrueckte_Taste = pg.key.get_pressed()         
                                     #wir schauen: wurde Pfeiltaste nach oben gedrückt?
         if gedrueckte_Taste[pg.K_UP] and self.position.y > 400:
@@ -94,7 +145,7 @@ class player(pg.sprite.Sprite):             #Quelle für Erstellungshilfe = pyga
 
 
     def shoot(self):
-        bullet = Projectile(100, 100, 20)
+        bullet = Projectile(100, 100, 20)'''
         
        
             
@@ -110,11 +161,12 @@ class Projectile(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (x,y)
 
-spieler = player(20, 20, 5, 5)
-spieler2 = player(20, 20, 10, 10)
-
+spieler = Player(20, 20, 5, 5)
+spieler2 = Player(20, 20, 10, 10)
+spiel = Game()
+spiel.new_game()
 #Main Loop:
-while True:
+'''while True:
     exit_game()
     spieler2.update()
     spieler.update() 
@@ -125,5 +177,4 @@ while True:
     screen.blit(spieler.image, spieler.position)
     
     pg.display.update()
-    FPS.tick(60)
-
+    FPS.tick(60)'''
