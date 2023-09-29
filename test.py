50ab558b210045d190a87708efd32f18695f11fc
D0dizzle <domenico-ferraro@web.de>
2023-09-29 12:12:16 +0200

    nichts relevantes

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 """Das hier ist für verschiedene Tests gekapert, nicht wundern!
 Hier gibt's ne kleine Default MainLoop, in der man Funktionen ausprobieren kann"""
 
-#wenn das hier klappt ist gut
-
 import pygame as pg
 import sys
 import random
@@ -13,30 +11,6 @@ from pygame.sprite import*
 
 game_folder = os.path.dirname(__file__)
 #TODO os.path.join um os path zu verallgrmrinrtn
-
-
-'''background1 = pg.image.load(os.path.join(game_folder, "Assets", "hintergrund", "parallax-background.png")).convert()
-background = pg.transform.scale(background1, (1000, 1000))
-background2 = pg.image.load(os.path.join(game_folder,"Assets","hintergrund" ,"parallax-space-ring-planet.png")).convert_alpha()
-background3 = pg.image.load(os.path.join(game_folder,"Assets","hintergrund","parallax-space-stars.png")).convert_alpha()
-background4 = pg.transform.scale(background3, (1000, 1000))'''
-
-# Background Asset Quelle: https://opengameart.org/content/space-background-3
-
-
-#Funktion, um Tastendruck des Spielers auszuwerten und entsprechende Aktionen auszuführen
-#kommt später vermutlich als Methode in die "Player"-Klasse
-
-"""def update(a):                                     #a ist die derzeitige Position des Player-Sprites
-    gedrueckte_Taste = pg.key.get_pressed()            
-    if gedrueckte_Taste[pg.K_UP]:                   #wir schauen: wurde Pfeiltaste nach oben gedrückt?
-        a.move_ip(0, -5)                            #wenn ja: bewegen wir den Spieler um -5 Pixel nach oben
-    if gedrueckte_Taste[pg.K_DOWN]:                  # hier 5 pixel nach unten
-        a.move_ip(0, 5)
-    if gedrueckte_Taste[pg.K_LEFT]:                 #-5 pixel nach links
-        a.move_ip(-5, 0)
-    if gedrueckte_Taste[pg.K_RIGHT]:                #5 pixel nach rechts
-        a.move_ip(5, 0)"""
 
 #Klassen:
 class Game:
@@ -52,7 +26,6 @@ class Game:
         self.background4 = pg.transform.scale(self.background3, (1000, 1000))
 
         # Background Asset Quelle: https://opengameart.org/content/space-background-3
-
 
     def new_game(self):
         self.sprites = pg.sprite.Group()
@@ -95,9 +68,7 @@ class Game:
         self.screen.blit(self.sprites)
         self.screen.blit(self.players)
         self.screen.blit(self.bullets)
-
-
-        
+       
     def exit_game():
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -118,37 +89,7 @@ class Player(pg.sprite.Sprite):             #Quelle für Erstellungshilfe = pyga
         self.rect = self.image.get_rect()
         self.rect.center = (250, 400)
         self.position = self.rect
-        
-        
-    
-    '''def update(self):
-        gedrueckte_Taste = pg.key.get_pressed()         
-                                    #wir schauen: wurde Pfeiltaste nach oben gedrückt?
-        if gedrueckte_Taste[pg.K_UP] and self.position.y > 400:
-            self.rect.move_ip(0, -1 * self.vy)                            #wenn ja: bewegen wir den Spieler um -5 Pixel nach oben
-        if gedrueckte_Taste[pg.K_DOWN] and self.position.y < 450:                     # hier 5 pixel nach unten
-            self.rect.move_ip(0, self.vy)
-        if gedrueckte_Taste[pg.K_LEFT] and self.position.x > 0:                 #-5 pixel nach links
-            self.rect.move_ip(-1 * self.vx, 0)
-        if gedrueckte_Taste[pg.K_RIGHT] and self.position.x < 450:                #5 pixel nach rechts
-            self.rect.move_ip(self.vx, 0)
-        if gedrueckte_Taste[pg.K_w] and self.position.y > 400:
-            self.rect.move_ip(0, -1 * self.vy)
-        if gedrueckte_Taste[pg.K_s] and self.position.y < 450:                     # hier 5 pixel nach unten
-            self.rect.move_ip(0, self.vy)
-        if gedrueckte_Taste[pg.K_a] and self.position.x > 0:                 #-5 pixel nach links
-            self.rect.move_ip(-1 * self.vx, 0) 
-        if gedrueckte_Taste[pg.K_d] and self.position.x < 450:                #5 pixel nach rechts
-            self.rect.move_ip(self.vx, 0)
-        if gedrueckte_Taste[pg.K_SPACE]:
-            self.shoot()
-
-
-    def shoot(self):
-        bullet = Projectile(100, 100, 20)'''
-        
-       
-            
+                        
 class Projectile(pg.sprite.Sprite):
     
     def __init__(self,x,y,vy):
@@ -161,20 +102,7 @@ class Projectile(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (x,y)
 
-spieler = Player(20, 20, 5, 5)
-spieler2 = Player(20, 20, 10, 10)
 spiel = Game()
 spiel.new_game()
-#Main Loop:
-'''while True:
-    exit_game()
-    spieler2.update()
-    spieler.update() 
-    screen.blit(background, (0,0))
-    screen.blit(background2, (20,50))
-    screen.blit(background4, (20,50))
-    screen.blit(spieler.image, spieler.position)
-    screen.blit(spieler.image, spieler.position)
-    
-    pg.display.update()
-    FPS.tick(60)'''
+spieler = Player(20, 20, 5, 5)
+spieler2 = Player(20, 20, 10, 10)
