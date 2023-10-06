94a9cc008834703f5a6f94a81b90b26a32cac333
L1zzard <lisa.sachau@student.fh-kiel.de>
2023-10-06 18:45:51 +0200

    kleine Änderungen

--- settings.py
+++ settings.py
@@ -1,28 +1,21 @@
 #Diese Datei ist für Settings/Einstellungen gedacht!
+#### Imports:   ####
 import pygame
 import os
 import sys
-FPS = pygame.time.Clock()
 
+#### Variablen: ####
 schwarz = (0, 0, 0)
 weiss = (255, 255, 255)
 cyan = (100, 100, 255)
 hoehe = 600
 breite = 800
+FPS = pygame.time.Clock()
 
 game_folder = os.path.dirname(__file__)
 screen = pygame.display.set_mode(size=(breite, hoehe))
 
-
-#Funktion zum Beenden des Spiels durch "x" in der Ecke
-def exit_game():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()                       #Quelle für pygame.quit(): coderslegacy.com/python/python-pygame-tutorial
-            sys.exit()
-
-
-#Hintergründe WARNUNG! 
+## Hintergründe: ##
 #vorab PNG's laden, die transformiert werden müssen
 background1 =  pygame.image.load(os.path.join(game_folder, "Assets", "hintergrund", "parallax-background.png")).convert()
 space_stars1 = pygame.image.load(os.path.join(game_folder,"Assets","hintergrund" ,"parallax-space-stars.png")).convert_alpha()
@@ -34,6 +27,17 @@ hg_dict["big-planet"] = pygame.image.load(os.path.join(game_folder,"Assets","hin
 hg_dict["far-planets"] = pygame.image.load(os.path.join(game_folder,"Assets","hintergrund", "parallax-space-far-planets.png")).convert_alpha()
 hg_dict["ring-planet"] = pygame.image.load(os.path.join(game_folder,"Assets","hintergrund", "parallax-space-ring-planet.png")).convert_alpha()
 
+
+#### Funktionen: ####
+#Funktion zum Beenden des Spiels durch "x" in der Ecke
+def exit_game():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()                       #Quelle für pygame.quit(): coderslegacy.com/python/python-pygame-tutorial
+            sys.exit()
+
+
+#### Klassen für Settings: ####
 class Hintergrund:
     def update():
         for i in hg_dict:
